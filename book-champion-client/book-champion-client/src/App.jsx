86d2a3001a5c3340c5ee1c6be1e3364f28441c4a
Selene <selenedanyluk@gmail.com>
2025-07-30 17:28:35 -0300

    Create project, bookitem component

---
+++ book-champion-client/book-champion-client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import BookItem from './components/bookItem/BookItem';
+
+function App() {
+  return (
+    <>
+      <h1>Book champions app</h1>
+      <p>Quiero leer libros!</p>
+      <BookItem />
+    </>
+  )
+}
+
+export default App
